7a4c4ca8b5ad7171b7eb8cd7636b784c24049e68
Hilal <41302444+htpck@users.noreply.github.com>
2020-12-22 19:16:35 +0300

    Add files via upload

---
+++ CarController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float fuel = 1;
+    public float fuelconsumption = 100f;
+    public Rigidbody2D carRigidBody;
+    public Rigidbody2D backTire;
+    public Rigidbody2D frontTire;
+    public float speed = 20;
+    public float carTorque = 10;
+    public float movement;
+    public UnityEngine.UI.Image image;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        image.fillAmount = fuel;
+    }
+
+    private void FixedUpdate()
+    {
+        if (fuel > 0)
+        {
+            backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            carRigidBody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
+        }
+        fuel -= fuelconsumption * Mathf.Abs(movement) * Time.fixedDeltaTime;
+        }
+}
---
+++ addFuel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addFuel : MonoBehaviour
+{
+    public CarController carController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        carController.fuel = 1;
+        Destroy(gameObject);
+    }
+
+}
